1c74b9b8c72330d88050b0fe0682227b822d6c73
avinash <avinashgbadiger@gmail.com>
2024-11-27 23:21:07 +0530

    This is updated POM.xml for Registry in EUREKA

--- src/main/java/com/sof_tech/user_service/MicroserviceProjectApplication.java
+++ src/main/java/com/sof_tech/user_service/MicroserviceProjectApplication.java
@@ -3,6 +3,7 @@ package com.sof_tech.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MicroserviceProjectApplication {
 
